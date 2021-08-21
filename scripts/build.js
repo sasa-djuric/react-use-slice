bf6c8553a55b04f93ae20dbaf151598bb69fe3b0
Saša Đurić <djsasa.mail@gmail.com>
2021-08-21 20:42:31 +0200

    Build script improvement

--- scripts/build.js
+++ scripts/build.js
@@ -1,19 +1,24 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
+const packageJson = require('../package.json');
 const { execSync } = require('child_process');
 
 const distPath = path.resolve(process.cwd(), 'lib');
 
-(() => {
+(async () => {
 	execSync('tsc');
 
-	fs.copyFileSync(
-		path.resolve(process.cwd(), 'package.json'),
-		path.resolve(distPath, 'package.json')
+	delete packageJson.scripts.prepare;
+
+	const packagePromise = fs.writeFile(
+		path.resolve(distPath, 'package.json'),
+		JSON.stringify(packageJson, null, 4)
 	);
 
-	fs.copyFileSync(
+	const readmePromise = fs.copyFile(
 		path.resolve(process.cwd(), 'README.md'),
 		path.resolve(distPath, 'README.md')
 	);
+
+	await Promise.all([packagePromise, readmePromise]);
 })();
